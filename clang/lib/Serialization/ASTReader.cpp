9ccefd496e04feb8a8d518307945a928b404ec23
Gui Andrade <gh@archshift.com>
2020-06-02 11:41:18 -0700

    ASTReader: Don't leave ObjCInterface EndLoc uninitialized

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -6719,7 +6719,7 @@ void TypeLocReader::VisitPackExpansionTypeLoc(PackExpansionTypeLoc TL) {
 }
 
 void TypeLocReader::VisitObjCInterfaceTypeLoc(ObjCInterfaceTypeLoc TL) {
-  TL.setNameLoc(readSourceLocation());
+  TL.initializeLocal(Reader.getASTContext(), readSourceLocation());
 }
 
 void TypeLocReader::VisitObjCTypeParamTypeLoc(ObjCTypeParamTypeLoc TL) {
